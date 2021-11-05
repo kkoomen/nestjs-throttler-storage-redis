52470fac523c6ad84f3de79e4dac4905f41b8009
Mickael <m.pomet@mesdocteurs.com>
2021-11-05 14:23:36 +0100

    fix: disconnect redis to avoid open handles

--- src/throttler-storage-redis.service.ts
+++ src/throttler-storage-redis.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
 import * as Redis from 'ioredis';
 import { ThrottlerStorageRedis } from './throttler-storage-redis.interface';
 
 @Injectable()
-export class ThrottlerStorageRedisService implements ThrottlerStorageRedis {
+export class ThrottlerStorageRedisService implements ThrottlerStorageRedis, OnModuleDestroy {
   redis: Redis.Redis;
+  disconnectRequired?: boolean;
   scanCount: number;
 
   constructor(redis?: Redis.Redis, scanCount?: number);
@@ -17,8 +18,10 @@ export class ThrottlerStorageRedisService implements ThrottlerStorageRedis {
       this.redis = redisOrOptions;
     } else if (typeof redisOrOptions === 'string') {
       this.redis = new Redis(redisOrOptions as string);
+      this.disconnectRequired = true;
     } else {
       this.redis = new Redis(redisOrOptions);
+      this.disconnectRequired = true;
     }
   }
 
@@ -29,5 +32,11 @@ export class ThrottlerStorageRedisService implements ThrottlerStorageRedis {
 
   async addRecord(key: string, ttl: number): Promise<void> {
     await this.redis.set(`${key}:${Date.now() + ttl * 1000}`, ttl, 'EX', ttl);
+  }
+
+  onModuleDestroy() {
+    if (this.disconnectRequired) {
+      this.redis?.disconnect(false);
+    }
   }
 }
