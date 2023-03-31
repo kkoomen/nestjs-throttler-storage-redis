b3da3367ffeb78b6567a859a3631f9a71c50073e
Kim 金可明 <koomen@protonmail.com>
2023-03-31 20:17:20 +0200

    chore: try to use 0.0.0.0 rather than 127.0.0.1

--- test/utility/redis-cluster.ts
+++ test/utility/redis-cluster.ts
@@ -1,12 +1,12 @@
 import { Cluster } from 'ioredis';
 
 export const clusterNodes = [
-  { host: '127.0.0.1', port: 7000 },
-  { host: '127.0.0.1', port: 7001 },
-  { host: '127.0.0.1', port: 7002 },
-  { host: '127.0.0.1', port: 7003 },
-  { host: '127.0.0.1', port: 7004 },
-  { host: '127.0.0.1', port: 7005 },
+  { host: '0.0.0.0', port: 7000 },
+  { host: '0.0.0.0', port: 7001 },
+  { host: '0.0.0.0', port: 7002 },
+  { host: '0.0.0.0', port: 7003 },
+  { host: '0.0.0.0', port: 7004 },
+  { host: '0.0.0.0', port: 7005 },
 ];
 
 export const cluster = new Cluster(clusterNodes);
