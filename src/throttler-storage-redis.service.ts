7f0784d75cecf29350a24ff902a7c42c23bcdf9d
sdg9670 <sdg9670@naver.com>
2024-07-18 13:48:29 +0900

    feat: upgrade @nestjs/throttler to v6, add new throttling features

--- src/throttler-storage-redis.service.ts
+++ src/throttler-storage-redis.service.ts
@@ -31,38 +31,72 @@ export class ThrottlerStorageRedisService implements ThrottlerStorageRedis, OnMo
     // Credits to wyattjoh for the fast implementation you see below.
     // https://github.com/wyattjoh/rate-limit-redis/blob/main/src/lib.ts
     return `
-      local totalHits = redis.call("INCR", KEYS[1])
-      local timeToExpire = redis.call("PTTL", KEYS[1])
-      if timeToExpire <= 0
-        then
-          redis.call("PEXPIRE", KEYS[1], tonumber(ARGV[1]))
-          timeToExpire = tonumber(ARGV[1])
-        end
-      return { totalHits, timeToExpire }
+      local hitKey = KEYS[1]
+      local blockKey = KEYS[2]
+      local throttlerName = ARGV[1]
+      local ttl = tonumber(ARGV[2])
+      local limit = tonumber(ARGV[3])
+      local blockDuration = tonumber(ARGV[4])
+
+      local totalHits = redis.call('INCR', hitKey)
+      local timeToExpire = redis.call('PTTL', hitKey)
+      
+      if timeToExpire <= 0 then
+        redis.call('PEXPIRE', hitKey, ttl)
+        timeToExpire = ttl
+      end
+
+      local isBlocked = redis.call('GET', blockKey)
+      local timeToBlockExpire = 0
+
+      if isBlocked then
+        timeToBlockExpire = redis.call('PTTL', blockKey)
+      elseif totalHits > limit then
+        redis.call('SET', blockKey, 1, 'PX', blockDuration)
+        isBlocked = '1'
+        timeToBlockExpire = blockDuration
+      end
+
+      if isBlocked and timeToBlockExpire <= 0 then
+        redis.call('DEL', blockKey)
+        redis.call('SET', hitKey, 1, 'PX', ttl)
+        totalHits = 1
+        timeToExpire = ttl
+        isBlocked = false
+      end
+
+      return { totalHits, timeToExpire, isBlocked and 1 or 0, timeToBlockExpire }
     `
       .replace(/^\s+/gm, '')
       .trim();
   }
 
-  async increment(key: string, ttl: number): Promise<ThrottlerStorageRecord> {
-    // Use EVAL instead of EVALSHA to support both redis instances and clusters.
+  async increment(
+    key: string,
+    ttl: number,
+    limit: number,
+    blockDuration: number,
+    throttlerName: string,
+  ): Promise<ThrottlerStorageRecord> {
+    const hitKey = `${this.redis.options.keyPrefix}{${key}:${throttlerName}}:hits`;
+    const blockKey = `${this.redis.options.keyPrefix}{${key}:${throttlerName}}:blocked`;
     const results: number[] = (await this.redis.call(
       'EVAL',
       this.scriptSrc,
-      1,
-      `${this.redis.options.keyPrefix}${key}`,
+      2,
+      hitKey,
+      blockKey,
+      throttlerName,
       ttl,
+      limit,
+      blockDuration,
     )) as number[];
 
     if (!Array.isArray(results)) {
       throw new TypeError(`Expected result to be array of values, got ${results}`);
     }
 
-    if (results.length !== 2) {
-      throw new Error(`Expected 2 values, got ${results.length}`);
-    }
-
-    const [totalHits, timeToExpire] = results;
+    const [totalHits, timeToExpire, isBlocked, timeToBlockExpire] = results;
 
     if (typeof totalHits !== 'number') {
       throw new TypeError('Expected totalHits to be a number');
@@ -72,9 +106,19 @@ export class ThrottlerStorageRedisService implements ThrottlerStorageRedis, OnMo
       throw new TypeError('Expected timeToExpire to be a number');
     }
 
+    if (typeof isBlocked !== 'number') {
+      throw new TypeError('Expected isBlocked to be a number');
+    }
+
+    if (typeof timeToBlockExpire !== 'number') {
+      throw new TypeError('Expected timeToBlockExpire to be a number');
+    }
+
     return {
       totalHits,
       timeToExpire: Math.ceil(timeToExpire / 1000),
+      isBlocked: isBlocked === 1,
+      timeToBlockExpire: Math.ceil(timeToBlockExpire / 1000),
     };
   }
 
